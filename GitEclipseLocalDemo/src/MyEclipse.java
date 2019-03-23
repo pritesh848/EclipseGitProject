fa1446240133c63c3bd526038237d105a75dea35
pritesh848 <pritesh848@gmail.com>
2019-03-23 15:35:00 +0530

    inutial old

--- GitEclipseLocalDemo/src/MyEclipse.java
+++ GitEclipseLocalDemo/src/MyEclipse.java
@@ -2,7 +2,7 @@
 public class MyEclipse {
  public static void main (String[]args)  {
 	 System.out.println("my first commit the command 2");
-	 System.out.println(oold branch commit);
+	 System.out.println("ooold branch commit");
  }
 
 }
